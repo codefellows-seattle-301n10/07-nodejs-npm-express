e15f2d49e3dcfacd76a0a97fb9316927195d39a9
Mrebb <madhu.rebbana@gmail.com>
2018-03-31 14:04:00 -0700

    comments updated in server.js

--- madhu-tama-jackie/server.js
+++ madhu-tama-jackie/server.js
@@ -4,18 +4,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
+// REVIEWED: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
 app.get('/articles', (req,res) => {
   res.sendFile('index.html',{root: './public'});
 })
 //define directory for serving files for all methods
 app.use(express.static('public'));
-app.get('/new.html', (req,res) => {
+//Create a get request to handle new html web page
+app.get('/new-article', (req,res) => {
   res.sendFile('new.html',{root: './public'});
 })
+//Posting the article to middleware
 app.post('/articles', express.urlencoded(), function(request, response) {
-  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  // REVIEWED: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 })
+//Listen to port 3000
 app.listen(PORT, () => console.log('listening to port', PORT));
