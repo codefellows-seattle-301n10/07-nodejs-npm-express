ddc23cf92f507d3bc08bb7c3656947ceb5a31bc8
Tyler <Tconfalone@gmail.com>
2018-03-31 10:25:16 -0700

    refactored funtion

--- haley-tyler/server.js
+++ haley-tyler/server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 // COMMENT: Our files are in a public directory so that they can be accessible by the user. app.use() defines a middleware whose only purpose is to serve static files. 
 
 // REVIEW: POST route needs to parse the body passed in with the request. express.urlencoded() attaches "middleware" to do that
-app.post('/articles', express.urlencoded(), function(request, response) {
+app.post('/articles', express.urlencoded(),(request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
 
   console.log(request.body);
